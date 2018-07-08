67d6a1ff357f73bc626e6d0740df731ba1661ec1
Ivy Chen <fay.chenya@gmail.com>
2018-07-08 13:50:21 +0800

    Home Work Done

---
+++ HomeWork1/HomeWork1/Controllers/MoneyBookController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using HomeWork1.Models;
+
+namespace HomeWork1.Controllers
+{
+    public class MoneyBookController : Controller
+    {
+        // GET: MoneyBook
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult ListAll()
+        {
+            var fakeData = new List<MoneyBookViewModels>();
+            for (int i = 1; i < 51; i++)
+            {
+                var item = new MoneyBookViewModels
+                {
+                    No = i,
+                    BookDate = DateTime.Now.AddDays(-i),
+                    FeeType=(i%2==1 ? FeeTypes.支出 : FeeTypes.收入),
+                    Fee=i*100
+                };
+                fakeData.Add(item);
+            }
+            return View(fakeData);
+        }
+
+
+    }
+}
---
+++ HomeWork1/HomeWork1/Models/MoneyBookViewModels.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeWork1.Models
+{
+    public class MoneyBookViewModels
+    {
+        [DisplayName("#")]
+        public int No { get; set; }
+        [DisplayName("類別")]
+        public FeeTypes FeeType { get; set; }
+        
+        [DisplayName("金額")]
+        [DisplayFormat(DataFormatString ="{0:N0}")]
+        public int Fee { get; set; }
+        [DisplayName("日期")]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}")]
+        public DateTime BookDate { get; set; }
+
+        [DisplayName("備註")]
+        public string Remarks { get; set; }
+
+    }
+
+    public enum FeeTypes
+    {
+        收入 = 1,
+        支出 = 2
+    }
+   
+}
